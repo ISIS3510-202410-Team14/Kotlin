b07180a4640e40ff54240248181e4f20afe1905d
Angel9921 <la.angarita@uniandes.edu.co>
2024-03-22 13:37:37 -0500

    Uni View

---
+++ app/src/main/java/com/optic/moveon/view/UniversityActivity.kt
@@ -0,0 +1,12 @@
+package com.optic.moveon.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.optic.moveon.R
+
+class UniversityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_university)
+    }
+}
