7dc4ead91778f401aaf0f9049d79fb0fb3e5c4e7
Santiago Vela <74679743+svela0426@users.noreply.github.com>
2024-03-23 03:52:47 -0500

    update routing vistas_ detail of univeristies

--- app/src/main/java/com/optic/moveon/view/MainActivity.kt
+++ app/src/main/java/com/optic/moveon/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.optic.moveon.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import com.optic.moveon.R
 
@@ -9,5 +12,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val univer1= findViewById<ImageView>(R.id.univer1)
+        val univer2= findViewById<ImageView>(R.id.univer2)
+        val univer3= findViewById<ImageView>(R.id.univer3)
+        val univer4= findViewById<ImageView>(R.id.univer4)
+
+        univer1.setOnClickListener {
+            val intent= Intent(this,UniversityActivity3::class.java)
+            startActivity(intent)
+        }
+
+        univer2.setOnClickListener {
+            val intent= Intent(this,UniversityActivity2::class.java)
+            startActivity(intent)
+        }
+
+        univer3.setOnClickListener {
+            val intent= Intent(this,UniversityActivity::class.java)
+            startActivity(intent)
+        }
+
+        univer4.setOnClickListener {
+            val intent= Intent(this,UniversityActivity4::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
+
     }
 }
