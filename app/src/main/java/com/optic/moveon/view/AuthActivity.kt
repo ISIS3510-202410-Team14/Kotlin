f04abb208cc02176af3afba21b3c7fc636f01c9b
Angel9921 <la.angarita@uniandes.edu.co>
2024-04-02 16:41:24 -0500

    login y signup

--- app/src/main/java/com/optic/moveon/view/AuthActivity.kt
+++ app/src/main/java/com/optic/moveon/view/AuthActivity.kt
@@ -6,13 +6,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.optic.moveon.R
+import com.optic.moveon.databinding.ActivityLoginBinding
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+
+
+
         val btn_next= findViewById<Button>(R.id.btn_next)
 
         val textViewRegisterLink= findViewById<TextView>(R.id.textViewRegisterLink)
@@ -22,9 +39,28 @@ class AuthActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        textViewRegisterLink.setOnClickListener {
-            val intent= Intent(this,RegisterActivity::class.java)
+
+
+        binding.textViewRegisterLink.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.buttonLogin.setOnClickListener{
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Empty Fields Are not Allowed", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/optic/moveon/view/RegisterActivity.kt
+++ app/src/main/java/com/optic/moveon/view/RegisterActivity.kt
@@ -1,12 +1,51 @@
 package com.optic.moveon.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.optic.moveon.R
+import com.optic.moveon.databinding.ActivityRegisterBinding
+
 
 class RegisterActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+
+        binding.buttonRegister.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            val confirmPass = binding.confirmpassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPass.isNotEmpty()){
+                if (password == confirmPass){
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            val intent = Intent(this, AuthActivity::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, "JUMMMMM algo pasa", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else{
+                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Empty Fields Are not Allowed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
