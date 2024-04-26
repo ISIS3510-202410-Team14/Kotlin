bc55a67eeb194b24fa3138951c50daa149d28ac1
Santiago Vela <74679743+svela0426@users.noreply.github.com>
2024-04-26 13:15:51 -0500

    update form  per University

--- app/src/main/java/com/optic/moveon/model/entities/Chat.kt
+++ app/src/main/java/com/optic/moveon/model/entities/Chat.kt
@@ -7,6 +7,8 @@ data class Chat(
     val name: String?= null,
     val mensaje: String ?= null,
     val hora: Long ?= null,
-    var id: String ?= null
+    var id: String ?= null,
+    var enviado: Boolean = true
 ): Serializable
+
 
