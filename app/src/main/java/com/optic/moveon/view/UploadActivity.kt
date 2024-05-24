99bed8e2f9bd5f5582312d09b8544d6805cbe63e
Santiago Vela <74679743+svela0426@users.noreply.github.com>
2024-05-24 11:46:49 -0500

    update Image preview

--- app/src/main/java/com/optic/moveon/view/UploadActivity.kt
+++ app/src/main/java/com/optic/moveon/view/UploadActivity.kt
@@ -2,15 +2,18 @@ package com.optic.moveon.view
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.net.Uri
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.FirebaseStorage
-
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.database.FirebaseDatabase
 import com.optic.moveon.R
 import com.optic.moveon.databinding.ActivityUploadBinding
 
@@ -18,12 +21,14 @@ class UploadActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUploadBinding
 
-    private val PICK_FILE_REQUEST = 1
-    private lateinit var fileUri: Uri
+    private var fileUri: Uri? = null
     private lateinit var storageReference: StorageReference
     private lateinit var btnSelectFile: Button
     private lateinit var btnUploadFile: Button
     private lateinit var tvFileName: TextView
+    private lateinit var ivFilePreview: ImageView
+
+    private lateinit var selectFileLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +38,17 @@ class UploadActivity : AppCompatActivity() {
         btnSelectFile = binding.btnSelectFile
         btnUploadFile = binding.btnUploadFile
         tvFileName = binding.tvFileName
+        ivFilePreview = binding.ivFilePreview
 
         storageReference = FirebaseStorage.getInstance().reference
+
+        selectFileLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK && result.data != null && result.data?.data != null) {
+                fileUri = result.data?.data
+                tvFileName.text = fileUri?.path
+                showFilePreview(fileUri)
+            }
+        }
 
         btnSelectFile.setOnClickListener {
             selectFile()
@@ -43,44 +57,56 @@ class UploadActivity : AppCompatActivity() {
         btnUploadFile.setOnClickListener {
             uploadFile()
         }
-
-
-
-
-
     }
-
-
 
     private fun selectFile() {
         val intent = Intent().apply {
             type = "*/*"
             action = Intent.ACTION_GET_CONTENT
         }
-        startActivityForResult(Intent.createChooser(intent, "Selecciona un archivo"), PICK_FILE_REQUEST)
+        selectFileLauncher.launch(Intent.createChooser(intent, "Selecciona un archivo"))
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_FILE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
-            fileUri = data.data!!
-            tvFileName.text = fileUri.path
+    private fun showFilePreview(uri: Uri?) {
+        uri?.let {
+            val contentResolver = contentResolver
+            val mimeType = contentResolver.getType(uri)
+            if (mimeType?.startsWith("image/") == true) {
+                ivFilePreview.setImageURI(uri)
+                ivFilePreview.visibility = ImageView.VISIBLE
+            } else {
+                ivFilePreview.visibility = ImageView.GONE
+            }
         }
     }
 
     private fun uploadFile() {
-        if (::fileUri.isInitialized) {
-            val fileReference = storageReference.child("uploads/${System.currentTimeMillis()}.${getFileExtension(fileUri)}")
-            fileReference.putFile(fileUri)
+        fileUri?.let { uri ->
+            val fileReference = storageReference.child("uploads/${System.currentTimeMillis()}.${getFileExtension(uri)}")
+            fileReference.putFile(uri)
                 .addOnSuccessListener {
-                    Toast.makeText(this, "Archivo subido exitosamente", Toast.LENGTH_LONG).show()
+                    fileReference.downloadUrl.addOnSuccessListener { uri ->
+                        val downloadUrl = uri.toString()
+                        saveFileUrlToDatabase(downloadUrl)
+                    }
                 }
                 .addOnFailureListener { e ->
                     Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
                 }
-        } else {
+        } ?: run {
             Toast.makeText(this, "Por favor selecciona un archivo primero", Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun saveFileUrlToDatabase(downloadUrl: String) {
+        val databaseReference = FirebaseDatabase.getInstance().getReference("uploads")
+        databaseReference.child("val").setValue(downloadUrl)
+            .addOnSuccessListener {
+                Toast.makeText(this, "URL guardada en la base de datos", Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+            }
     }
 
     private fun getFileExtension(uri: Uri): String? {
@@ -89,4 +115,3 @@ class UploadActivity : AppCompatActivity() {
         return mime.getExtensionFromMimeType(contentResolver.getType(uri))
     }
 }
-
