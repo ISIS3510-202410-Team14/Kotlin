e1d745940fe80270f3f0085c523dca89566e852e
Santiago Vela <74679743+svela0426@users.noreply.github.com>
2024-05-25 18:41:24 -0500

    micro optimization strategies

--- app/src/main/java/com/optic/moveon/view/AdapterChat.kt
+++ app/src/main/java/com/optic/moveon/view/AdapterChat.kt
@@ -1,44 +1,35 @@
 package com.optic.moveon.view
 
 import android.content.Context
-import android.content.Intent
-import com.squareup.picasso.Picasso
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.optic.moveon.R
 import com.optic.moveon.model.entities.Chat
 
-class AdapterChat(private val context: Context, private val chatList : ArrayList<Chat>) :RecyclerView.Adapter<AdapterChat.MyViewHolder>(){
-
+class AdapterChat(private val context: Context, private val chatList: ArrayList<Chat>) : RecyclerView.Adapter<AdapterChat.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(
-            R.layout.chat_detail_item,
-            parent,false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.chat_detail_item, parent, false)
         return MyViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {
-        return chatList.size
-    }
+    override fun getItemCount(): Int = chatList.size
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-
-        val currentitem =chatList[position]
-        holder.nombre.text = currentitem.name
-        holder.texto.text = currentitem.mensaje
-
+        val currentItem = chatList[position]
+        holder.bind(currentItem)
     }
 
-    class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val nombre: TextView = itemView.findViewById(R.id.textViewSenderName)
+        private val texto: TextView = itemView.findViewById(R.id.textViewMessage)
 
-        val nombre: TextView= itemView.findViewById(R.id.textViewSenderName)
-        val texto: TextView= itemView.findViewById(R.id.textViewMessage)
+        fun bind(chat: Chat) {
+            nombre.text = chat.name
+            texto.text = chat.mensaje
+        }
     }
-
-
 }
